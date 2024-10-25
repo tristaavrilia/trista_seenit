c43cf3858183ad4da952e936f32e4c4ad64fe60f
Tajmirul <tasmirolislam@gmail.com>
2024-10-26 03:59:32 +0600

    update

--- actions/watchlist.ts
+++ actions/watchlist.ts
@@ -6,7 +6,6 @@ export const addToWatchlist = (movieId: number) => {
 
     if (!watchlist.includes(movieId)) {
         watchlist.push(movieId);
-        console.log('adding to watchlist', watchlist);
 
         setCookie('watchlist', JSON.stringify(watchlist), {
             maxAge: 10 * 24 * 60 * 60, // 10 days
@@ -18,7 +17,6 @@ export const removeFromWatchlist = (movieId: number) => {
     const watchlist = getWatchlist();
 
     const updatedWatchlist = watchlist.filter((id) => id !== movieId);
-    console.log('removing from watchlist', updatedWatchlist);
     setCookie('watchlist', JSON.stringify(updatedWatchlist), {
         maxAge: 10 * 24 * 60 * 60, // 10 days
     });
