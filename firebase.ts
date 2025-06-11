1b7e225c62d97c785cc6503b6814d3f6e2528d24
tristaavrilia <tristaavrilia@gmail.com>
2025-06-11 16:08:19 +0700

    coba sentry 6

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,7 @@
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtzxluD-670lfHCbbqweVayAPIx6CNe0o",
   authDomain: "devops-trista.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
   appId: "1:164514167755:web:04733beb62663b782d544e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-// ✅ Tambahkan ini supaya bisa di-import
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
