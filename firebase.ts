b6a1ae9d52c0f69ea49141a701c3fb762e25f481
tristaavrilia <tristaavrilia@gmail.com>
2025-06-11 15:53:36 +0700

    coba sentry 4

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCtzxluD-670lfHCbbqweVayAPIx6CNe0o",
+  authDomain: "devops-trista.firebaseapp.com",
+  projectId: "devops-trista",
+  storageBucket: "devops-trista.firebasestorage.app",
+  messagingSenderId: "164514167755",
+  appId: "1:164514167755:web:04733beb62663b782d544e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
